a73272175186ae6d1c37f39a9961eba753ffd084
El Senpai <El Senpai@EL-SENPAI>
2021-06-24 08:51:03 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,51 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> _categoryDal = new GenericRepository<Category>();
+
+
+        //private ICategoryDal _categoryDal;
+        //public CategoryManager(ICategoryDal categoryDal)
+        //{
+        //    _categoryDal = categoryDal;
+        //}
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.List();
+        }
+
+        public void add(Category category)
+        {
+            if (CheackCategory(category))
+            {
+              _categoryDal.Add(category);
+            }
+            
+        }
+
+        private bool CheackCategory(Category category)
+        {
+            if (category.CategoryName==""||category.CategoryName.Length<=3||category.Description==""||category.CategoryName.Length>= 51)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+
+}
---
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class, new()
+    {
+        Context c = new Context();
+        DbSet<T> _object;
+        public GenericRepository()
+        {
+            _object = c.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            _object.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _object.Remove(entity);
+            c.SaveChanges();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter = null)
+        {
+            return filter == null
+                 ? _object.ToList()
+                 : _object.Where(filter).ToList();
+        }
+
+        public void Update(T entity)
+        {
+            c.SaveChanges();
+        }
+    }
+}
