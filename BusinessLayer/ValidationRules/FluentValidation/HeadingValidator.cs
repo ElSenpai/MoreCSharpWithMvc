9db46f02e3b6701f17bf2eb9773c43c184712d05
El Senpai <El Senpai@EL-SENPAI>
2021-07-07 15:18:07 +0300

    viewbad and dropdown

---
+++ BusinessLayer/ValidationRules/FluentValidation/HeadingValidator.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules.FluentValidation
+{
+   public class HeadingValidator : AbstractValidator<Heading>
+    {
+        public HeadingValidator()
+        {
+            RuleFor(h => h.HeadingName).NotEmpty().WithMessage("Bu alanı boş bırakamazsın");
+        }
+    }
+}
--- Mvc/Controllers/HeadingController.cs
+++ Mvc/Controllers/HeadingController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules.FluentValidation;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +12,41 @@ namespace Mvc.Controllers
 {
     public class HeadingController : Controller
     {
-        // GET: Heading
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        HeadingValidator hv = new HeadingValidator();
         public ActionResult Index()
         {
+            var headingvalues = hm.GetList();
+            return View(headingvalues);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> valueCategory = (from x in cm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text=x.CategoryName,
+                                                      Value=x.CategoryId.ToString()
+                                                  }).ToList();
+            List<SelectListItem> valueWriter = (from x in wm.GetList()
+                                                  select new SelectListItem
+                                                  {
+                                                      Text = x.FirstName+" "+x.LastName,
+                                                      Value = x.WriterId.ToString()
+                                                  }).ToList();
+
+            ViewBag.vlc = valueCategory;
+            ViewBag.vlw = valueWriter;
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            heading.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            hm.Add(heading);
+            return RedirectToAction("Index");
         }
     }
 }
